b6d1bfdef01ca2ce58737591d9c078af9786f703
sathishselvamani <sathishnbc@gmail.com>
2021-07-06 10:29:31 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,31 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter word: ");
+		String str = in.next();
+
+
+		System.out.println("Middle = "  + middle(str));
+		} //end main
+
+		public static String middle(String str)
+		{
+		    if ((str.length() % 2) == 0)
+		    {
+
+		        if (str.length() > 2) {
+		        	return str.substring(str.length()/2 - 1, str.length()/2 + 1);
+		        	}
+		        else {return str;}
+		    }
+		    else {
+		    	return str.substring(str.length()/2);
+		    	}
+		}   // end middle
+}
